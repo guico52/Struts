06c59f46eee47381ebc4a3334f68b95ca0f19e9d
guico52 <1305497489@qq.com>
2022-12-16 22:33:28 +0800

    实验六完成

--- src/main/java/DAO/TeacherDao.java
+++ src/main/java/DAO/TeacherDao.java
@@ -1,60 +1,40 @@
 package DAO;
 
 import bean.Teanchinfo;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import java.util.List;
 
 public class TeacherDao {
-    private static final SessionFactory sessionFactory = HibernateSessionFactory.getSessionFactory();
+    private static HibernateTemplate template;
 
 //    查询所有教师信息
     public static List<Teanchinfo> getAllTeachers(){
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        List<Teanchinfo> list = session.createQuery("from Teanchinfo").list();
-        transaction.commit();
-        session.close();
-        return list;
+        return template.loadAll(Teanchinfo.class);
     }
 
 //    根据id查询教师信息
     public static Teanchinfo getTeacherById(String id){
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Teanchinfo teanchinfo = session.get(Teanchinfo.class, id);
-        transaction.commit();
-        session.close();
-        return teanchinfo;
+        return template.get(Teanchinfo.class, id);
     }
 
 //    添加教师信息
     public static void addTeacher(Teanchinfo teanchinfo){
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(teanchinfo);
-        transaction.commit();
-        session.close();
+        teanchinfo.setId(teanchinfo.getId());
     }
 
 //    删除教师信息
     public static void deleteTeacher(String id){
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Teanchinfo teanchinfo = session.get(Teanchinfo.class, id);
-        session.delete(teanchinfo);
-        transaction.commit();
-        session.close();
+        Teanchinfo teanchinfo = template.get(Teanchinfo.class, id);
+        template.delete(teanchinfo);
     }
 
 //    更新教师信息
     public static void updateTeacher(Teanchinfo teanchinfo){
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.update(teanchinfo);
-        transaction.commit();
-        session.close();
+        template.update(teanchinfo);
+    }
+
+    public void setTemplate(HibernateTemplate template) {
+        TeacherDao.template = template;
     }
 }
